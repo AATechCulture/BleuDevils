d1b9a9ef15e1ffce91f7b901b7e274423a1133eb
djo534 <yassinetn534@gmail.com>
2023-11-05 10:00:03 -0600

    o

---
+++ main.py
@@ -0,0 +1,143 @@
+from flask import Flask, jsonify, request, render_template
+import nlpcloud, os
+from flask_cors import CORS
+import json
+import braintree
+
+app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+CORS(app)
+
+# Initialize the NLP Cloud client
+client = nlpcloud.Client("nllb-200-3-3b", os.getenv('TRANSLATOR_API_KEY'))
+
+# client2 = currencyapicom.Client(os.getenv('CURRENCY_API_KEY'))
+# result = client2.latest('USD',currencies=['AUD','EUR'])
+# print(result)
+
+# Sample data for demonstration (replace this with your actual data storage logic)
+# menu_items = [
+#     {"id": 1, "name": "Popcorn", "price": 5.99},
+#     {"id": 2, "name": "Cookies", "price": 3.99},
+#     {"id": 3, "name": "Sprite", "price": 10.99},
+#      Add more menu items as needed
+# ]
+from menus import menu_items_AA, menu_items_Frontier, menu_items_Southwest, menu_items_Delta, menu_items_Spirit, menu_items_BritishAirways,menu_items_Alaska, menu_items_Allegiant,all_menu_items
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/confirmation")
+def confirmation():
+    return render_template("confirmation_page.html")
+
+@app.route('/menuAA', methods=['GET'])
+def get_menu_AA():
+    airline_name = "American Airlines"
+    return render_template('menu.html', menu_items=menu_items_AA, airline_name=airline_name)
+@app.route('/menuDelta', methods=['GET'])
+def get_menu_Delta():
+    airline_name = "Delta Airlines"
+    return render_template('menu.html', menu_items=menu_items_Delta, airline_name=airline_name)
+@app.route('/menuSouthwest', methods=['GET'])
+def get_menu_Southwest():
+    airline_name = "Southwest Airlines"
+    return render_template('menu.html', menu_items=menu_items_Southwest, airline_name=airline_name)
+@app.route('/menuSpirit', methods=['GET'])
+def get_menu_Spirit():
+    airline_name = "Spirit Airlines"
+    return render_template('menu.html', menu_items=menu_items_Spirit, airline_name=airline_name)
+@app.route('/menuFrontier', methods=['GET'])
+def get_menu_Frontier():
+    airline_name = "Frontier Airlines"
+    return render_template('menu.html', menu_items=menu_items_Frontier, airline_name=airline_name)
+@app.route('/menuAlaska', methods=['GET'])
+def get_menu_Alaska():
+    airline_name = "Alaska Airlines"
+    return render_template('menu.html', menu_items=menu_items_Alaska, airline_name=airline_name)
+@app.route('/menuBritish', methods=['GET'])
+def get_menu_British():
+    airline_name = "British Airways"
+    return render_template('menu.html', menu_items=menu_items_BritishAirways, airline_name=airline_name)
+@app.route('/menuAllegiant', methods=['GET'])
+def get_menu_Allegiant():
+    airline_name = "Allegiant Airways"
+    return render_template('menu.html', menu_items=menu_items_Allegiant, airline_name=airline_name)
+
+@app.route('/translate', methods=['GET'])
+def translate():
+    translated_menu = []
+    for item in menu_items_AA:
+        try:
+            translated_name = client.translation(item['name'], source='eng_Latn', target='spa_Latn')  # Translate from English to Spanish
+            translated_item = {"name": translated_name, "price": item['price']}
+            translated_menu.append(translated_item)
+        except Exception as e:
+            print(f"Error translating item '{item['name']}': {e}")
+    return render_template('menu.html', menu_items=translated_menu)
+
+
+selected_menu_items = []
+
+# # Configure Braintree API credentials (replace with your actual credentials)
+# braintree.Configuration.configure(
+#     braintree.Environment.Sandbox,
+#     merchant_id='jjhycvgsf33h3hzv',
+#     public_key='rs36fybb4dg368vb',
+#     private_key='56612fc4d57d57164d6e7c31b298b900'
+# )
+
+# @app.route('/check')
+# def index():
+#     return render_template('test.html')
+
+# @app.route('/client_token', methods=['GET'])
+# def generate_client_token():
+#     client_token = braintree.ClientToken.generate()
+#     return jsonify({'client_token': client_token})
+
+# @app.route('/process_payment', methods=['POST'])
+# def process_payment():
+#     nonce = request.json['paymentMethodNonce']
+#     result = braintree.Transaction.sale({
+#         "amount": "10.00",  # Replace with the actual amount
+#         "payment_method_nonce": nonce,
+#         "options": {
+#             "submit_for_settlement": True
+#         }
+#     })
+
+#     if result.is_success:
+#         return jsonify({'success': True, 'message': 'Payment successful'})
+#     else:
+#         return jsonify({'success': False, 'message': 'Payment failed'})
+
+
+@app.route('/submit', methods=['POST'])
+def submit_order():
+    menu_item_ids = request.form.getlist('menu_item')
+    global selected_menu_items
+    selected_menu_items = []
+
+    for menu_item_id in menu_item_ids:
+        selected_item = next((item for item in all_menu_items if item['id'] == int(menu_item_id)), None)
+        if selected_item:
+            selected_menu_items.append(selected_item)
+
+    total_amount = calculate_order_amount(menu_item_ids)
+    total_price = total_amount / 100 
+    return render_template('order.html', order_items=selected_menu_items, total_price=total_price)
+
+def calculate_order_amount(selected_item_ids):
+    total_amount = 0
+    for item_id in selected_item_ids:
+        selected_item = next((item for item in all_menu_items if item['id'] == int(item_id)), None)
+        if selected_item:
+            total_amount += selected_item['price']
+    return total_amount * 100
+    
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ menus.py
@@ -0,0 +1,105 @@
+all_menu_items = []
+
+menu_items_AA = [
+    {"id": 1, "name": "Wine and sparkling wine", "price": 9},
+    {"id": 2, "name": "Spirits (50ml)", "price": 10},
+    {"id": 3, "name": "Beer", "price": 8},
+    {"id": 4, "name": "Select Harvest Sea Salt Roasted Almonds", "price": 8},
+    {"id": 5, "name": "Pringles Original", "price": 4},
+    {"id": 6, "name": "Breakfast bag", "price": 7},
+    {"id": 7, "name": "Fruit and cheese plate", "price": 11},
+    # Add more menu items as needed
+    
+]
+
+
+
+menu_items_Frontier = [
+    {"id": 1, "name": "Wine and Champagne", "price": 9},
+    {"id": 2, "name": "Spirits", "price": 10},
+    {"id": 3, "name": "Beer", "price": 8},
+    {"id": 4, "name": "Roasted Almonds", "price": 8},
+    {"id": 5, "name": "Pringles", "price": 4},
+    {"id": 6, "name": "Breakfast Bag", "price": 7},
+    {"id": 7, "name": "Fruit and Cheese Plate", "price": 11},
+    # Add more menu items as needed
+]
+
+
+menu_items_Southwest = [
+    {"id": 1, "name": "Deep Eddy Vodka", "price": 7},
+    {"id": 2, "name": "Cruset Brut Sparkling Wine", "price": 6},
+    {"id": 3, "name": "Miller Lite", "price": 6},
+    {"id": 4, "name": "High Noon Hard Seltzer—Pineapple", "price": 7},
+    # Add more menu items as needed
+]
+
+
+menu_items_Delta = [
+    {"id": 1, "name": "Bistro Snack Box", "price": 10},
+    {"id": 2, "name": "Market Snack Box", "price": 10},
+    {"id": 3, "name": "Chicken Salad Sandwich Plate", "price": 12},
+    {"id": 4, "name": "Fruit and Cheese Plate", "price": 12},
+    {"id": 5, "name": "Bacardi Rum", "price": 10},
+    {"id": 6, "name": "Tip Top Espresso Martini", "price": 12},
+    {"id": 7, "name": "Voodoo Ranger", "price": 9},
+    {"id": 8, "name": "Une Femme 'The Callie' Sparkling Rosé", "price": 10},
+    # Add more menu items as needed
+]
+
+
+menu_items_Spirit = [
+    {"id": 1, "name": "Tea", "price": 3},
+    {"id": 2, "name": "Sodas", "price": 4},
+    {"id": 3, "name": "Water", "price": 4},
+    {"id": 4, "name": "Heineken", "price": 9},
+    {"id": 5, "name": "Margaritaville Gold Tequila", "price": 10},
+    {"id": 6, "name": "M&M’s Peanut, Share Size", "price": 5},
+    {"id": 7, "name": "Pringles", "price": 5},
+    {"id": 8, "name": "Signature Snack Box", "price": 9},
+    # Add more menu items as needed
+]
+
+menu_items_Alaska = [
+    {"id": 1, "name": "Straightaway Margarita", "price": 12},
+    {"id": 2, "name": "Domaine St. Vincent Sparkling Wine", "price": 9},
+    {"id": 3, "name": "Jack Daniel's Whiskey", "price": 8},
+    {"id": 4, "name": "Signature Fruit & Cheese Platter", "price": 9},
+    {"id": 5, "name": "Kid's Choice Picnic Pack", "price": 7},
+    {"id": 6, "name": "Mediterranean Tapas Picnic Pack", "price": 7},
+    {"id": 7, "name": "The Jetsetter's Jam Sandwich", "price": 8},
+    # Add more menu items as needed
+]
+
+menu_items_BritishAirways = [
+    {"id": 1, "name": "Soda", "price": 2},
+    {"id": 2, "name": "Vodka", "price": 6},
+    {"id": 3, "name": "Vinca Organic Pale Rosé", "price": 6},
+    {"id": 4, "name": "Twinings Tea", "price": 3},
+    {"id": 5, "name": "Haribo Starmix", "price": 3},
+    {"id": 6, "name": "Ploughman's Veggie Snack Box", "price": 6},
+    {"id": 7, "name": "Bacon Stone Baked Roll", "price": 5},
+    # Add more menu items as needed
+]
+
+menu_items_Allegiant = [
+    {"id": 1, "name": "Vodka Lemonade", "price": 12},
+    {"id": 2, "name": "Mimosa", "price": 15},
+    {"id": 3, "name": "Maragarita Combo", "price": 19},
+    {"id": 4, "name": "Coffee", "price": 5},
+    {"id": 5, "name": "Welch's Mixed Fruit Snacks", "price": 5},
+    {"id": 6, "name": "Wingz Kids Box", "price": 11},
+    {"id": 7, "name": "Bistro Box", "price": 11},
+    # Add more menu items as needed
+]
+
+
+all_menu_items.extend(menu_items_AA)
+all_menu_items.extend(menu_items_Frontier)
+all_menu_items.extend(menu_items_Southwest)
+all_menu_items.extend(menu_items_BritishAirways)
+all_menu_items.extend(menu_items_Allegiant)
+all_menu_items.extend(menu_items_Alaska)
+all_menu_items.extend(menu_items_Spirit)
+all_menu_items.extend(menu_items_Delta)
+
